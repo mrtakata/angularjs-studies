c311d92701ced5279eba46fb8569451d0e98295b
mrtakata <matheustakata@Matheuss-MacBook-Pro.local>
2015-06-11 17:57:59 -0300

    Initial commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,9 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+angular.module('F1FeederApp', [
+  'F1FeederApp.controllers',
+  'F1FeederApp.services'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
